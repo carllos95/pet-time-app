0dcac34a03101f902c764623406f48aad49bc114
carlosd <carlosd@clubegazetadopovo.com.br>
2024-10-12 16:45:08 -0300

    feat: create theme

---
+++ src/theme/index.ts
@@ -0,0 +1,23 @@
+import { extendTheme } from "native-base";
+
+export const THEME = extendTheme({
+  colors: {
+    white: '#fff',
+    black: '#000',
+    pink: {
+      400: '#A1005C'
+    }
+  },
+  fontSizes: {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 20,
+  },
+  sizes: {
+    14: 56,
+    33: 148
+  }
+
+})
